c9375e2bdbe020ad869a029c4627ec9b14117ad0
DiverBro <serginar@ucm.es>
2025-05-04 02:55:18 +0200

    aVANCES

---
+++ Proyecto_Final_DSI/Assets/Scripts/GameMenuPestanyas.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameMenuPestanyas : MonoBehaviour
+{
+    VisualElement button_characters;
+    VisualElement button_game;
+    VisualElement pestanya_characters;
+    VisualElement pestanya_game;
+    private void NoContenido()
+    {
+        pestanya_characters.style.display = DisplayStyle.None;
+        pestanya_game.style.display = DisplayStyle.None;
+    }
+
+    private void OnEnable()
+    {
+        UIDocument uidoc = GetComponent<UIDocument>();
+        VisualElement rootve = uidoc.rootVisualElement;
+
+        VisualElement button = rootve.Q("BelowButtons");
+        VisualElement pestanya = rootve.Q("Content");
+
+        pestanya_characters = pestanya.Q("MenuInicial");
+        pestanya_game = pestanya.Q("InitialMenu");
+
+        button_characters = button.Q("Characters");
+        button_game = button.Q("Game");
+
+        button_characters.RegisterCallback<MouseDownEvent>(evt =>
+        {
+            NoContenido();
+            pestanya_characters.style.display = DisplayStyle.Flex;
+        });
+
+        button_game.RegisterCallback<MouseDownEvent>(evt =>
+        {
+            NoContenido();
+            pestanya_game.style.display = DisplayStyle.Flex;
+        });
+
+
+    }
+}
